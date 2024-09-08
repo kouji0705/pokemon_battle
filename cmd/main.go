7c55950f6abc3abf0ac22db9fe7b60339a312483
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-09-08 19:57:24 +0900

    add:

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/username/pokemon-battle/internal/core"
+)
 
 func main() {
-	fmt.Println("Welcome to Pokemon Battle!")
+	// ポケモンの作成
+	pikachu := &core.Pokemon{
+		Name:    "Pikachu",
+		HP:      100,
+		Attack:  55,
+		Defense: 40,
+		Speed:   90,
+		Moves: []core.Move{
+			{Name: "Thunderbolt", Power: 90, Accuracy: 95, Type: "Electric"},
+		},
+	}
+
+	charmander := &core.Pokemon{
+		Name:    "Charmander",
+		HP:      100,
+		Attack:  52,
+		Defense: 43,
+		Speed:   65,
+		Moves: []core.Move{
+			{Name: "Flamethrower", Power: 90, Accuracy: 95, Type: "Fire"},
+		},
+	}
+
+	// バトルの開始
+	battle := &core.Battle{Pokemon1: pikachu, Pokemon2: charmander}
+	result := battle.Start()
+	println(result)
 }
